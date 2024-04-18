58060d6b89b23c92cef0a2ff43d2c93721c448af
Brian <bwook9908@gmail.com>
2024-04-18 20:13:14 +0900

    vault backup: 2024-04-18 20:13:14

---
+++ src/implementation/SingleLinkedList/SingleListSample.c
@@ -0,0 +1,19 @@
+//
+// Created by Yoon BeongWook on 4/18/24.
+//
+
+#include "stdio.h"
+
+typedef struct NODE {
+    // 관리될 데이터
+    int nData;
+
+    // 데이터 관리를 위한 포인터
+    struct NODE* next;
+} NODE;
+
+int SingleList()
+{
+    NODE node = {0};
+    return 0;
+}
