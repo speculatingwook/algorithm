7d2e046a63f4682d5d82f1949299f1ca4dd59356
Brian <bwook9908@gmail.com>
2024-04-18 21:00:14 +0900

    vault backup: 2024-04-18 21:00:14

--- src/implementation/SingleLinkedList/SingleListSample.c
+++ src/implementation/SingleLinkedList/SingleListSample.c
@@ -16,17 +16,19 @@ int SingleList()
 {
     NODE list[5] = { 0 };
 
+    list[0].nData = 100;
+    list[1].nData = 200;
+    list[2].nData = 300;
+    list[3].nData = 400;
+    list[4].nData = 500;
+
+    // 연결 리스트 구조화
     list[0].next = &list[1];
     list[1].next = &list[2];
     list[2].next = &list[3];
     list[3].next = &list[4];
     list[4].next = NULL;
 
-    list[0].nData = 100;
-    list[1].nData = 200;
-    list[2].nData = 300;
-    list[3].nData = 400;
-    list[4].nData = 500;
 
     for(int i = 0; i < 5; ++i)
         printf("list[%d]: %d\n", i, list[i].nData);
