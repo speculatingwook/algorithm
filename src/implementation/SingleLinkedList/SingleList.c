5f9e9ca7257f8159c7b6578d7c89e2d29a98fc82
Brian <bwook9908@gmail.com>
2024-04-19 17:45:36 +0900

    vault backup: 2024-04-19 17:45:36

--- src/implementation/SingleLinkedList/SingleList.c
+++ src/implementation/SingleLinkedList/SingleList.c
@@ -1,12 +1,26 @@
 //
 // Created by Yoon BeongWook on 4/19/24.
 //
+#include "stdio.h"
 
 typedef struct NODE
 {
+    char szData[64];
     struct NODE* next;
 } NODE;
 
+NODE* g_pHead = NULL;
+
+// 연결 리스트 전체 데이터 출력
+void printList(void){
+
+    while(g_pHead != NULL){
+        printf("[%p] %s, next[%p]", g_pHead, g_pHead -> szData, g_pHead -> next);
+        g_pHead = g_pHead -> next;
+    }
+}
+
 int SingleList(){
 
+    return 0;
 }
