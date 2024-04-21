acc997a48e41c7c4c89f468c81fc942189e5fe2b
Brian <bwook9908@gmail.com>
2024-04-21 16:04:14 +0900

    vault backup: 2024-04-21 16:04:14

--- src/implementation/stack/StackPractice.c
+++ src/implementation/stack/StackPractice.c
@@ -1,93 +1,86 @@
-//
-// Created by Yoon BeongWook on 4/21/24.
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct NODE
-{
+typedef struct NODE {
     char szData[64];
     struct NODE* next;
 } NODE;
 
-NODE* stack_head = {0};
+NODE* stack_head = NULL;
 
-
-int IsStackEmpty()
-{
-    if (stack_head->next == NULL)
-        return 1;
-    return 0;
+int IsStackEmpty() {
+    return (stack_head == NULL || stack_head->next == NULL);
 }
 
-// 전체 데이터 출력
-void printStackList(void){
-    NODE* pHead = stack_head;
-    while(pHead != NULL){
+void printStackList(void) {
+    NODE* pHead = stack_head->next;
+    while (pHead != NULL) {
         printf("[%p] %s, next[%p]\n", pHead, pHead->szData, pHead->next);
         pHead = pHead->next;
     }
 }
-//
-//void ReleaseList(void)
-//{
-//    printf("\nReleaseList()\n");
-//    NODE* pTmp = g_pHead->next;
-//    while(pTmp != NULL) {
-//        NODE* pDelete = pTmp;
-//        pTmp = pTmp -> next;
-//
-//        printf("Delete: [%p] %s\n", pDelete, pDelete->szData);
-//        free(pDelete);
-//    }
-//    g_pHead->next = 0;
-//}
 
-int PushData(char* pszData)
-{
+void ReleaseList(void) {
+    NODE* pTmp = stack_head->next;
+    while (pTmp != NULL) {
+        NODE* pDelete = pTmp;
+        pTmp = pTmp->next;
+        printf("Delete: [%p] %s\n", pDelete, pDelete->szData);
+        free(pDelete);
+    }
+    free(stack_head);
+    stack_head = NULL;
+}
+
+int PushData(char* pszData) {
     NODE* pNode = (NODE*)malloc(sizeof(NODE));
     memset(pNode, 0, sizeof(NODE));
-    strncpy(pNode-> szData, pszData, sizeof(pNode -> szData));
+    strncpy(pNode->szData, pszData, sizeof(pNode->szData));
 
-    if(IsStackEmpty())
+    if (stack_head == NULL) {
+        stack_head = (NODE*)malloc(sizeof(NODE));
+        memset(stack_head, 0, sizeof(NODE));
         stack_head->next = pNode;
-    else
-    {
-        pNode ->next = stack_head->next;
+    } else {
+        pNode->next = stack_head->next;
         stack_head->next = pNode;
     }
     return 1;
 }
 
-int PopData(NODE* pPopNode)
-{
-    NODE* stack_pointer = stack_head->next;
-    if(IsStackEmpty())
+int PopData(NODE* pPopNode) {
+    if (IsStackEmpty())
         return 0;
-    memcpy(pPopNode, stack_pointer, sizeof(NODE));
 
+    NODE* stack_pointer = stack_head->next;
+    memcpy(pPopNode, stack_pointer, sizeof(NODE));
     stack_head->next = stack_pointer->next;
     free(stack_pointer);
     return 1;
 }
 
-
-int Stack(){
-    //Stack 테스트를 위한 코드
-    printf("stack");
+int Stack() {
+    printf("stack\n");
     PushData("TEST01");
     PushData("TEST02");
     PushData("TEST03");
-//    NODE node = {0};
-//    PopData(&node);
-//    printf("Pop: %s\n", node.szData);
-//
-//    PopData(&node);
-//    printf("Pop: %s\n", node.szData);
-//
-//    PopData(&node);
-//    printf("Pop: %s\n", node.szData);
+
+    NODE node = {0};
+    PopData(&node);
+    printf("Pop: %s\n", node.szData);
+
+    PopData(&node);
+    printf("Pop: %s\n", node.szData);
+
+    PopData(&node);
+    printf("Pop: %s\n", node.szData);
+
+    ReleaseList();
+    return 0;
+}
+
+int main() {
+    Stack();
     return 0;
 }
